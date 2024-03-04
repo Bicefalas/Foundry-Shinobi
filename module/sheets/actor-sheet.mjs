d7f828b182850af2d0158e2771e88ab813dcf8fb
Bicefalas <158187402+Bicefalas@users.noreply.github.com>
2024-03-04 19:16:33 +0100

    Chat rolls fix, feature tab finish and css working properly.

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -47,7 +47,7 @@ export class ShinobiActorSheet extends ActorSheet {
     context.flags = actorData.flags;
 
     // Prepare character data and items.
-    if (actorData.type == 'character') {
+    if (actorData.type == 'shinobi') {
       this._prepareItems(context);
       this._prepareCharacterData(context);
     }
@@ -233,7 +233,7 @@ export class ShinobiActorSheet extends ActorSheet {
 
     // Handle rolls that supply the formula directly.
     if (dataset.roll) {
-      let label = dataset.label ? `[ability] ${dataset.label}` : '';
+      let label = dataset.label ? `${game.i18n.localize("Check of")} ${dataset.label}` : '';
       let roll = new Roll(dataset.roll, this.actor.getRollData());
       roll.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
